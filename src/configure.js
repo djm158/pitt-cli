2e68037afc9fc9bcec1534438aedc1f98a9426d2
Dan McGrath <djm158@pitt.edu>
2017-10-11 11:55:50 -0400

    node option doesn't make sense

--- src/configure.js
+++ src/configure.js
@@ -15,23 +15,6 @@ function configure(env) {
         console.error('Error, only Linux supported at this time');
         process.exit(-1);
     }
-
-    if(env === 'node') {
-        request.get('https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh')
-        .on('error', (err) => {
-            console.error(err);
-        })
-        .pipe(fs.createWriteStream('install.sh'));
-
-        exec('bash install.sh', (err, stdout, stderr) => {
-            if (err) {
-                console.error(`exec error: ${err}`);
-            }
-            console.log(`${stdout}`);
-        });
-
-    }
-
 }
 
 module.exports = conf;
