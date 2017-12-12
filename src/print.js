689d9ba57acf1dbbe1594b53eea188c252286801
Brian Choromanski <BrianChoromanski@gmail.com>
2017-12-12 00:32:40 -0500

    Cleaned up code, honestly don't know what I was doing

--- src/print.js
+++ src/print.js
@@ -26,60 +26,34 @@ function print(file, color) {
       console.error('Error:' + file + ' does not exist, please enter valid file path.');
       process.exit(-1);
    }
+   printer = 'mobileprint@pitt.edu';
    if(color){
-      creds.getCredentials((auth) => {
-         smptConfig.auth.user = auth.user + '@pitt.edu';
-         smptConfig.auth.pass = auth.pass;
-         let mailOptions = {
-            from: '<' + smptConfig.auth.user + '>',
-            to: 'colorprint@pitt.edu',
-            attachments: [
-               {
-                  filename: file,
-                  path: file
-               }
-            ]
-         };
-         let transporter = nodemailer.createTransport(smptConfig);
-   
-         const spinner = ora('sending...').start();
-         transporter.sendMail(mailOptions, (error, info) => {
-            spinner.succeed();
-            if (error) {
-               console.log(error);
-            }
-   
-            console.log(info);
-         });
-      });
-   }else{
-      creds.getCredentials((auth) => {
-         smptConfig.auth.user = auth.user + '@pitt.edu';
-         smptConfig.auth.pass = auth.pass;
-         let mailOptions = {
-            from: '<' + smptConfig.auth.user + '>',
-            to: 'mobileprint@pitt.edu',
-            attachments: [
-               {
-                  filename: file,
-                  path: file
-               }
-            ]
-         };
-         let transporter = nodemailer.createTransport(smptConfig);
-   
-         const spinner = ora('sending...').start();
-         transporter.sendMail(mailOptions, (error, info) => {
-            spinner.succeed();
-            if (error) {
-               console.log(error);
-            }
-   
-            console.log(info);
-         });
-      });
+       printer = 'colorprint@pitt.edu';
    }
+   creds.getCredentials((auth) => {
+      smptConfig.auth.user = auth.user + '@pitt.edu';
+      smptConfig.auth.pass = auth.pass;
+      let mailOptions = {
+         from: '<' + smptConfig.auth.user + '>',
+         to: printer,
+         attachments: [
+            {
+               filename: file,
+               path: file
+            }
+         ]
+      };
+      let transporter = nodemailer.createTransport(smptConfig);
 
+      const spinner = ora('sending...').start();
+      transporter.sendMail(mailOptions, (error, info) => {
+         spinner.succeed();
+         if (error) {
+            console.log(error);
+         }
+            console.log(info);
+      });
+   });
 }
 
 module.exports = printer;
